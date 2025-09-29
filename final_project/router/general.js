c98eb1cf11367753ed9ce65c7660c4d2b6e97098
Ramil Aquino Jr. <ramilaquinojr@gmail.com>
2025-09-29 21:02:41 +0800

    update 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,6 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios');
 
 // Register a new user
 public_users.post("/register", (req, res) => {
@@ -21,79 +22,47 @@ public_users.post("/register", (req, res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) { 
-  new Promise((resolve, reject) => {
-    if (books) {
-      resolve(books);
-    } else {
-      reject("No books found");
-    }
-  })
-  .then(result => {
-    res.send(JSON.stringify(result, null, 4));
-  })
-  .catch(error => {
-    res.status(500).json({ message: error });
+  let promise = new Promise((resolve, reject) => {
+    axios.get('http://localhost:5000')
+      .then(response => resolve(response.data))
+      .catch(error => reject(error));
   });
+  promise
+    .then(data => res.json(data))
+    .catch(error => res.status(500).json({ message: "Error fetching book list", error: error.message}));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  new Promise((resolve, reject) => {
-    if (books) {
-      resolve(books);
-    } else {
-      reject("No books found");
-    }
-  })
-  .then(result => {
-    let isbn = req.params.isbn;
-    res.send(result[isbn]);
-  })
-  .catch(error => {
-    res.status(500).json({ message: error });
-  });
+public_users.get('/isbn/:isbn', async function (req, res) {
+  let isbn = req.params.isbn;
+  try {
+    let response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(404).json({ message: "Book not found", error: error.message });
+  }
 });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  new Promise((resolve, reject) => {
-    if (books) {
-      resolve(books);
-    } else {
-      reject("No books found");
-    }
-  })
-  .then(result => {
-    let author = req.params.author.toLowerCase();
-    let filtered = Object.values(result).filter(book => 
-      book.author.toLowerCase() === author
-    );
-    res.send(JSON.stringify(filtered, null, 4));
-  })
-  .catch(error => {
-    res.status(500).json({ message: error });
-  });
+public_users.get('/author/:author', async function (req, res) {
+  try {
+    let author = req.params.author;
+    let response = await axios.get(`http://localhost:5000/author/${encodeURIComponent(author)}`);
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(404).json({ message: "Author not found", error: error.message });
+  }
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  new Promise((resolve, reject) => {
-    if (books) {
-      resolve(books);
-    } else {
-      reject("No books found");
-    }
-  })
-  .then(result => {
-    let title = req.params.title.toLowerCase();
-    let filtered = Object.values(result).filter(book => 
-      book.title.toLowerCase() === title
-    );
-    res.send(JSON.stringify(filtered, null, 4));
-  })
-  .catch(error => {
-    res.status(500).json({ message: error });
-  });
+public_users.get('/title/:title', async function (req, res) {
+  try {
+    let title = req.params.title;
+    let response = await axios.get(`http://localhost:5000/title/${encodeURIComponent(title)}`);
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(404).json({ message: "Title not found", error: error.message });
+  }
 });
 
 //  Get book review
